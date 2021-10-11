ac1279b5aba0d8fa0c85e641b3b1888dc59cbf76
Carlitos <carlosfer161105@gmail.com>
2021-10-11 12:15:38 -0500

    nuevo

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let A = document.getElementById("A");
+let S = document.getElementById("S");
+let D = document.getElementById("D");
+let F = document.getElementById("F");
+let G = document.getElementById("G");
+let H = document.getElementById("H");
+let J = document.getElementById("J");
+let K = document.getElementById("K");
+let L = document.getElementById("L");
+let aa = document.getElementById("body1");
+let ss = document.getElementById("body1");
+let dd = document.getElementById("body1");
+let ff = document.getElementById("body1");
+let gg = document.getElementById("body1");
+let hh = document.getElementById("body1");
+let jj = document.getElementById("body1");
+let kk = document.getElementById("body1");
+let ll = document.getElementById("body1");
+
+
+
+A.addEventListener("click", () =>{
+    clap.play();
+    aa.style.animationPlayState = "running";
+})
+S.addEventListener("click", () =>{
+    hithat.play();
+    ss.style.animationPlayState = "running";
+})
+D.addEventListener("click", () =>{
+    kick.play();
+    dd.style.animationPlayState = "running";
+
+})
+F.addEventListener("click", () =>{
+    openhat.play();
+    ff.style.animationPlayState = "running";
+
+})
+G.addEventListener("click", () =>{
+    boom.play();
+    gg.style.animationPlayState = "running";
+
+})
+H.addEventListener("click", () =>{
+    ride.play();
+    hh.style.animationPlayState = "running";
+
+})
+J.addEventListener("click", () =>{
+    snare.play();
+    jj.style.animationPlayState = "running";
+
+})
+K.addEventListener("click", () =>{
+    tom.play();
+    kk.style.animationPlayState = "running";
+
+})
+L.addEventListener("click", () =>{
+    tink.play();
+    ll.style.animationPlayState = "running";
+
+})
